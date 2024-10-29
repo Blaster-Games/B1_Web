5a1ae510ac8de36cf7dbaf7201c98de4ee11de39
K.Dongyoung <doyokim75@gmail.com>
2024-10-29 11:17:46 +0900

    241029 initial commit

---
+++ backend/blaster/src/main/java/org/kdr/blaster/BlasterApplication.java
@@ -0,0 +1,13 @@
+package org.kdr.blaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlasterApplication.class, args);
+	}
+
+}
---
+++ backend/blaster/src/test/java/org/kdr/blaster/BlasterApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kdr.blaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
