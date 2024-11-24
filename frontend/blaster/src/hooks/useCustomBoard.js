e8cde48f262bcadf643d7376b505325865badd2d
K.Dongyoung <doyokim75@gmail.com>
2024-11-24 14:08:10 +0900

    241124 게시판 요청 안가는 것 확인 중

--- frontend/blaster/src/hooks/useCustomBoard.js
+++ frontend/blaster/src/hooks/useCustomBoard.js
@@ -2,6 +2,7 @@ import useCustomMove from './useCustomMove';
 import { useEffect, useState } from 'react';
 import { postListGet } from '../api/boardApi';
 import { useParams } from 'react-router-dom';
+import { GAME, SORT } from '../constants/boardConstants';
 
 const initialPageInfo = {
   itemList: [],
@@ -16,11 +17,19 @@ const initialPageInfo = {
   next: true,
 };
 
+const initialRequestParam = {
+  page: 1,
+  size: 10,
+  game: GAME.BLASTER,
+  category: '',
+  sort: SORT.CREATED_AT,
+};
+
 const useCustomBoard = (RequestParam) => {
   const { category } = useParams();
   const { page, size, sort } = useCustomMove();
   const [pageInfo, setPageInfo] = useState(initialPageInfo);
-  const [requestParam, setRequestParam] = useState(RequestParam);
+  const [requestParam, setRequestParam] = useState(initialRequestParam);
 
   useEffect(() => {
     setRequestParam({
